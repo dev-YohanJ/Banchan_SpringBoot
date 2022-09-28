2767cf1e27957b5eba1c41db1c14749af73fdb08
Yohan Jeong <dev.yohanjeong@gmail.com>
2022-09-28 18:07:11 +0900

    0928 mac test2

--- Banchan_SpringBoot/src/main/java/com/BanchanApplication.java
+++ Banchan_SpringBoot/src/main/java/com/BanchanApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BanchanApplication {
-	//테스트
+	//테스트3
 	public static void main(String[] args) {
 		SpringApplication.run(BanchanApplication.class, args);
 	}
