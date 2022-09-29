6f0213583c3036aa6d72ad9b34543f7b2fa8251d
Yohan Jeong <dev.yohanjeong@gmail.com>
2022-09-29 14:47:23 +0900

    0929 test5

--- Banchan_SpringBoot/src/main/java/com/BanchanApplication.java
+++ Banchan_SpringBoot/src/main/java/com/BanchanApplication.java
@@ -3,6 +3,7 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test5
 @SpringBootApplication
 public class BanchanApplication {
 	public static void main(String[] args) {
