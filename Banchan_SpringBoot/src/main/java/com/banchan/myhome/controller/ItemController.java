9431eec2378ff233d2e38b52d9010b0f28f39afe
SUUBINN <sscssb555@gmail.com>
2022-10-14 09:07:33 +0900

    1004_1

--- Banchan_SpringBoot/src/main/java/com/banchan/myhome/controller/ItemController.java
+++ Banchan_SpringBoot/src/main/java/com/banchan/myhome/controller/ItemController.java
@@ -73,7 +73,7 @@ private static final Logger logger = LoggerFactory.getLogger(ItemController.clas
 		
 		//오라클 디비에 저장될 파일 명
 		//String fileDBName = "/" + year + "-" + month + "-" + date + "/" + refileName;
-		String fileDBName = File.separator + year + "-" + month + "-" + date + File.separator + refileName;
+		String fileDBName = year + "-" + month + "-" + date + "/" + refileName;
 		logger.info("fileDBName = " + fileDBName);
 		return fileDBName;
 	}
@@ -109,10 +109,10 @@ private static final Logger logger = LoggerFactory.getLogger(ItemController.clas
 			
 			
 			String fileDBName = fileDBName(fileName, saveFolder);
-			logger.info("UploadfileDBName = " + fileDBName);
+			logger.info("UploadfileDBName = " + "/" + fileDBName);
 			
 			// transferTo(File path) : 업로드한 파일을 매개변수의 경로에 저장합니다.
-			loadfile.transferTo(new File(saveFolder + fileDBName));
+			loadfile.transferTo(new File(saveFolder + "/" + fileDBName));
 			uploadfilenames += fileDBName + ",";
 			
 			}
@@ -213,7 +213,7 @@ private static final Logger logger = LoggerFactory.getLogger(ItemController.clas
 		MultipartFile[] uploadfile = itemdata.getUploadfile();
 		logger.info("갯수 : " + uploadfile.length);
 		
-		String uploadfilenames = null;
+		String uploadfilenames = "";
 		
 		if (check != null && !check.equals("")) { //기존파일 그대로 사용하는 경우입니다.
 			logger.info("기존파일 그대로 사용합니다." + check);
