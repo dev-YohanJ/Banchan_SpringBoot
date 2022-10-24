b317d8848b6101a4ad058d1b6493fe79f8d74dd2
dev-YohanJ <dev.yohanjeong@gmail.com>
2022-10-24 12:04:52 +0900

    test

--- Banchan_SpringBoot/src/main/java/com/banchan/myhome/controller/ErrorProcess.java
+++ Banchan_SpringBoot/src/main/java/com/banchan/myhome/controller/ErrorProcess.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ErrorProcess implements ErrorController {
 	private static final Logger logger = LoggerFactory.getLogger(ErrorProcess.class);
-	
+	 
 	@GetMapping(value = "/error")
 	public String goIndex() {
 		logger.info("error");
